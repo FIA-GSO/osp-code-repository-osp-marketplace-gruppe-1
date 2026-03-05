0067e096229fae95c151b6605a5abbb7d99b77ad
Roberto Natale <natale.roberto@web.de>
2026-03-05 09:57:35 +0100

    [TASK] fixes responsive view for template of listing booths

--- static/scripts/booths.js
+++ static/scripts/booths.js
@@ -47,12 +47,12 @@ document.querySelectorAll('.event-date').forEach(el => {
     const text = el.textContent.trim();
 
     if (text) {
-        const parts = text.split(' - ');
+        const parts = text.split(' ');
         const date = new Date(parts[0]);
 
         if (!isNaN(date)) {
             const formattedDate = date.toLocaleDateString('de-DE');
-            el.textContent = `${formattedDate} - ${parts.slice(1).join(' - ')}`;
+            el.textContent = `${formattedDate} ${parts.slice(1).join(' ')}`;
         }
     }
 });
