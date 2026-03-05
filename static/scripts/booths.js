9ebad3b5a524d52f9a5ed03eb8d387efb7505148
AurelGSO <aurel.f@gso.schule.koeln>
2026-03-05 14:30:28 +0100

    [TASK] outsource eventDateFormatter js

--- static/scripts/booths.js
+++ static/scripts/booths.js
@@ -56,17 +56,3 @@ document.querySelectorAll('.js-button').forEach(btn =>
         }
     })
 );
-
-document.querySelectorAll('.event-date').forEach(el => {
-    const text = el.textContent.trim();
-
-    if (text) {
-        const parts = text.split(' ');
-        const date = new Date(parts[0]);
-
-        if (!isNaN(date)) {
-            const formattedDate = date.toLocaleDateString('de-DE');
-            el.textContent = `${formattedDate} ${parts.slice(1).join(' ')}`;
-        }
-    }
-});
--- static/scripts/vocationalFairRegistrationForm.js
+++ static/scripts/vocationalFairRegistrationForm.js
@@ -1,16 +1,3 @@
-document.querySelectorAll('.event-date').forEach(el => {
-    const text = el.textContent.trim();
-
-    if (text) {
-        const parts = text.split(' ');
-        const date = new Date(parts[0]);
-
-        if (!isNaN(date)) {
-            const formattedDate = date.toLocaleDateString('de-DE');
-            el.textContent = `${formattedDate} ${parts.slice(1).join(' ')}`;
-        }
-    }
-});
 document.querySelectorAll('.event-date-checkbox').forEach(el => {
     const dateString = el.textContent.trim();
     if (dateString) {
